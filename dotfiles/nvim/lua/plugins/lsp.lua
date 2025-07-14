48b031fd71c60489f148d3278387e88795f14ac5
Pottarr <pottarrpongs@gmail.com>
2025-07-14 11:55:07 +0700

    Added noice.nvim config

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -1,68 +1,73 @@
 -- For NixOS ONLY because of the Filesystem Hierarchy Standard Problem ;-;
 
 return {
-  "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local lspconfig = require("lspconfig")
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local lspconfig = require("lspconfig")
 
-    -- Setup each LSP server manually (because no Mason)
-    lspconfig.bashls.setup({ mason = false, })
-    lspconfig.clangd.setup({ mason = false, })
-    lspconfig.gopls.setup({ mason = false, })
-    lspconfig.jdtls.setup({
-      -- on_attach = function(client, bufnr)
-      on_attach = function(_, bufnr)
-        -- Disable diagnostics for this buffer (Java only)
-        vim.diagnostic.enable(true, bufnr)
-      end,
-    })
-    lspconfig.nixd.setup({ mason = false, })
-    lspconfig.pyright.setup({ mason = false, })
-    lspconfig.tsserver.setup({ mason = false, })
-    -- require('render-markdown').setup({
-    --     completions = { lsp = { enabled = true } },
-    -- })
+        -- Setup each LSP server manually (because no Mason)
+        lspconfig.bashls.setup({ mason = false, })
+        lspconfig.clangd.setup({ mason = false, })
+        lspconfig.gopls.setup({ mason = false, })
+        lspconfig.jdtls.setup({
+        -- on_attach = function(client, bufnr)
+        on_attach = function()
+            -- Disable diagnostics for this buffer (Java only)
+            vim.diagnostic.config({
+                update_in_insert = false,
+            --     virtual_text = true,
+            --     signs = true,
+            --     underline = true,
+            })
+        end,
+        mason = false,
+        })
+        lspconfig.nixd.setup({ mason = false, })
+        lspconfig.pyright.setup({ mason = false, })
+        lspconfig.tsserver.setup({ mason = false, })
+        -- require('render-markdown').setup({
+        --     completions = { lsp = { enabled = true } },
+        -- })
 
-    -- Lua LS config
-    lspconfig.lua_ls.setup({
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" },
-          },
+        -- Lua LS config
+        lspconfig.lua_ls.setup({
+        settings = {
+            Lua = {
+            diagnostics = {
+                globals = { "vim" },
+            },
+            },
         },
-      },
-    })
+        })
 
-    -- Rust analyzer with diagnostics disabled
-    lspconfig.rust_analyzer.setup({
-      settings = {
-        ["rust-analyzer"] = {
-          diagnostics = {
-            enable = true,
-          },
+        lspconfig.rust_analyzer.setup({
+        settings = {
+            ["rust-analyzer"] = {
+            diagnostics = {
+                enable = true,
+            },
+            },
         },
-      },
-    })
+        })
 
-    -- You can add more servers here...
+        -- You can add more servers here...
 
-    vim.diagnostic.config({
-      virtual_text = true,
-      signs = true,
-      underline = true,
-      update_in_insert = true,
-    })
+        vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        underline = true,
+        update_in_insert = true,
+        })
 
 
-    lspconfig.gopls.setup({
-      -- on_attach = function(client, bufnr)
-      on_attach = function(_, bufnr)
-        -- Go to definition
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr })
-      end,
-    })
-  end,
+        lspconfig.gopls.setup({
+        -- on_attach = function(client, bufnr)
+        on_attach = function(_, bufnr)
+            -- Go to definition
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr })
+        end,
+        })
+    end,
 }
 
