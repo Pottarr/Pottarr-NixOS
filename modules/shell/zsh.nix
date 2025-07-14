faa14bf5a2fb079057d3b0834bc9baf26a4ea7ee
Pottarr <pottarrpongs@gmail.com>
2025-07-14 16:37:48 +0700

    Add powerlevel10k config file

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -1,18 +1,23 @@
 { config, pkgs, ... }:
 
 {
-  programs.zsh = {
-    enable = true;
-    oh-my-zsh = {
-      enable = true;
-      theme = "powerlevel10k/powerlevel10k";
-      plugins = [ "git" ];
-      custom = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k";
+    programs.zsh = {
+        enable = true;
+        oh-my-zsh = {
+        enable = true;
+        theme = "powerlevel10k/powerlevel10k";
+        plugins = [ "git" ];
+        custom = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k";
+        };
+        initContent = ''
+        [[ -f ${config.home.homeDirectory}/dotfiles/powerlevel10k/.p10k.zsh ]] && source ${config.home.homeDirectory}/dotfiles//zsh/.p10k.zsh
+        '';
     };
-  };
 
-  home.packages = with pkgs; [
-    zsh-powerlevel10k
-  ];
+    home.file.".p10k.zsh".source = ./../../dotfiles/powerlevel10k/.p10k.zsh;
+
+    home.packages = with pkgs; [
+        zsh-powerlevel10k
+    ];
 }
 
