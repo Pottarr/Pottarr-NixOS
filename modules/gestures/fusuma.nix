8ba862c1c01955d09b2fd3a87fecf8441647e96b
Pottarr <pottarrpongs@gmail.com>
2025-07-29 13:27:23 +0700

    Modified fusuma.nix

--- modules/gestures/fusuma.nix
+++ modules/gestures/fusuma.nix
@@ -1,9 +1,14 @@
-
-{ ... }:
+{ pkgs, ... }:
 
 let
-    dotfiles = ../../../dotfiles;
+    dotfiles = ../../dotfiles;
 in {
+    services.fusuma = {
+        enable = true;
+        package = with pkgs; [
+            fusuma
+        ];
+    };
     # Copy your alacirtty config file
     home.file.".config/fusuma/config.yml".source = "${dotfiles}/fusuma/config.yml";
 }
