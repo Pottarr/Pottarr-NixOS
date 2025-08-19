249faeea09a01582ec1693955383791c8f430e9f
Pottarr <pottarrpongs@gmail.com>
2025-08-19 22:33:53 +0700

    Added picom.conf

--- modules/window-manager/picom.nix
+++ modules/window-manager/picom.nix
@@ -1,5 +1,10 @@
-{...}:
+{ ... }:
 
+let
+    dotfiles = ../../dotfiles;
+in
 {
     services.picom.enable = true;
+    xdg.configFile."picom/picom.conf".source = "${dotfiles}/picom/picom.conf";
+    # home.file.".config/picom/picom.conf".source = "${dotfiles}/picom/picom.conf";
 }
