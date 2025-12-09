268d4c2e429d7b8c6e8625195c3f39420adf3d55
Pottarr <pottarrpongs@gmail.com>
2025-12-09 22:37:47 +0700

    Added Shortcut for QT-Designer

---
+++ dotfiles/scripts/open_qt_designer.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+DESIGER_PATH=which designer
+
+xdg-open DESIGER_PATH
--- modules/shortcuts/desktop.nix
+++ modules/shortcuts/desktop.nix
@@ -5,5 +5,9 @@ let
 in {
     home.file.".local/share/applications/Microsoft-Teams.desktop".source = "${dotfiles}/desktop/Microsoft-Teams.desktop";
     home.file.".local/share/applications/Microsoft-Teams.desktop".executable = true;
+    home.file.".local/share/applications/open_qt_designer".source = "${dotfiles}/scripts/open_qt_designer.sh";
+    home.file.".local/share/applications/open_qt_designer".executable = true;
+    home.file.".local/share/applications/QT-Designer.desktop".source = "${dotfiles}/desktop/QT-Designer.desktop";
+    home.file.".local/share/applications/QT-Designer.desktop".executable = true;
 }
 
